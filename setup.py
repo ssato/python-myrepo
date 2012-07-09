0bca097050369cb7d114b419ef2bd3c645870e51
Satoru SATOH <satoru.satoh@gmail.com>
2012-07-09 22:31:25 +0900

    0.2.11-rc1

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ curdir = os.getcwd()
 sys.path.append(curdir)
 
 PACKAGE = "myrepo"
-VERSION = "0.2.10.2"
+VERSION = "0.2.11"
 
 # daily snapshots:
 #VERSION = VERSION + datetime.datetime.now().strftime(".%Y%m%d")
