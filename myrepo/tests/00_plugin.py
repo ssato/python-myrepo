a9113543403d4edd02fd6114a5dcfa43b4d73d5e
Satoru SATOH <satoru.satoh@gmail.com>
2012-07-20 12:48:12 +0900

    added test cases for myrepo.hooks

--- myrepo/tests/00_plugin.py
+++ myrepo/tests/00_plugin.py
@@ -1,1 +1,17 @@
+import sys
+
 A = 0
+
+
+def __log_info(fn, *args, **kwargs):
+    print >> sys.stderr, "called %s w/ args=%s, kwargs=%s" % \
+        (fn, str(args), str(kwargs))
+
+
+def pre_init(*args, **kwargs):
+    __log_info("pre_init", *args, **kwargs)
+
+
+def post_init(*args, **kwargs):
+    __log_info("post_init", *args, **kwargs)
+
