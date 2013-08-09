bfe10ff544a64197ec7c4fc6fddf88aed6018fc8
Satoru SATOH <satoru.satoh@gmail.com>
2013-08-09 11:06:05 +0900

    update the simple app test framework and test cases

--- tests/driver.sh
+++ tests/driver.sh
@@ -12,5 +12,8 @@ tests=$testsdir/*.conf
 
 for f in $tests; do
     source $f
-    myrepo $MYREPO_TEST_ARGS
+    test "x$MYREPO_TEST_ARGS" = "x" || myrepo $MYREPO_TEST_ARGS
+    test "x$MYREPO_TEST_ARGS_1" = "x" || myrepo $MYREPO_TEST_ARGS_1
+    test "x$MYREPO_TEST_ARGS_2" = "x" || myrepo $MYREPO_TEST_ARGS_2
+    test "x$MYREPO_TEST_ARGS_3" = "x" || myrepo $MYREPO_TEST_ARGS_3
 done
