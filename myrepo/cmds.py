51c81d8ce36135835478bef40df55ca48644ad6f
Satoru SATOH <satoru.satoh@gmail.com>
2013-08-21 00:20:34 +0900

    implemented functions to make command strings for deploy, update and init (--no-genconf), and corresponding test cases

--- myrepo/cmds.py
+++ myrepo/cmds.py
@@ -52,9 +52,8 @@ def mk_cmds_to_build_srpm(repo, srpm, noarch=None, bdist=None):
 
 _MK_SYMLINKS_TO_NOARCH_RPM = """\
 for arch in %(other_archs_s)s; do \
-    (cd $arch/ && (for f in ../%(primar_arch)s/%(noarch_rpms)s; do \
-        ln -sf $f ./; done)); done
-"""
+(cd $arch/ && (for f in ../%(primary_arch)s/%(noarch_rpms)s; do \
+ln -sf $f ./; done)); done"""
 
 
 def mk_cmds_to_deploy_rpms(repo, srpm, noarch=None, bdist=None):
@@ -77,25 +76,58 @@ def mk_cmds_to_deploy_rpms(repo, srpm, noarch=None, bdist=None):
         bdist = repo.dist
 
     dcmd = repo.server.deploy_cmd
-    c0 = dcmd(os.path.join(rpmdir, "*.src.rpm"),
+    rpmdir = "/var/lib/mock/%s-%%s/result/" % bdist
+    c0 = dcmd(os.path.join(rpmdir % repo.primary_arch, "*.src.rpm"),
               os.path.join(repo.destdir, "sources"))
 
     if noarch:
-        rpmdir = "/var/lib/mock/%s-%s/result/" % (bdist, repo.primary_arch)
-
+        rpmdir = rpmdir % repo.primary_arch
         ctx = dict(other_archs_s=' '.join(repo.other_archs),
-                   primar_arch=repo.primar_arch,
+                   primary_arch=repo.primary_arch,
                    noarch_rpms="*.noarch.rpm")
-        (sc, sc_dir) = repo.adjust_cmd(_MK_SYMLINKS_TO_NOARCH_RPM % ctx)
+        (sc, sc_dir) = repo.adjust_cmd(_MK_SYMLINKS_TO_NOARCH_RPM % ctx,
+                                       repo.destdir)
 
         bc = SH.bind(dcmd(os.path.join(rpmdir, "*.noarch.rpm"),
-                          os.path.join(repo.destdir, repo.primary_arch)), sc)
+                          os.path.join(repo.destdir, repo.primary_arch)),
+                     sc)[0]
         cs = [c0, bc]
     else:
-        cs = [c0] + [dcmd(os.path.join(rpmdir, "*.%s.rpm" % a),
+        cs = [c0] + [dcmd(os.path.join(rpmdir % a, "*.%s.rpm" % a),
                           os.path.join(repo.destdir, a)) for a in repo.archs]
 
     return cs
+
+
+_UPDATE_REPO_METADATA = """\
+test -d repodata \
+&& createrepo --update --deltas --oldpackagedirs . --database . \
+|| createrepo --deltas --oldpackagedirs . --database ."""
+
+
+def mk_cmds_to_update(repo, *args, **kwargs):
+    """
+    Make up list of command strings to update repo metadata.
+
+    :param repo: Repo instance
+    :return: List of commands to update repo metadata
+    """
+    return [c for c, _d in
+            (repo.adjust_cmd(_UPDATE_REPO_METADATA,
+                             os.path.join(repo.destdir, a)) for a in
+             repo.archs)]
+
+
+def mk_cmds_to_init__no_genconf(repo, *args, **kwargs):
+    """
+    Make up list of command strings to update repo metadata.
+
+    :param repo: Repo instance
+    :return: List of commands to update repo metadata
+    """
+    dirs_s = os.path.join(repo.destdir,
+                          "{%s}" % ','.join(repo.archs + ["sources"]))
+    return [repo.adjust_cmd("mkdir -p " + dirs_s)[0]]
 
 
 # vim:sw=4:ts=4:et:
--- myrepo/tests/cmds.py
+++ myrepo/tests/cmds.py
@@ -78,5 +78,116 @@ class Test_10(unittest.TestCase):
         self.assertEquals(cs[0], "mock -r %s-x86_64 %s" % (bdist, srpm))
         self.assertEquals(cs[1], "mock -r %s-i386 %s" % (bdist, srpm))
 
+    def test_20_mk_cmds_to_deploy_rpms__noarch(self):
+        repo = _REMOTE_REPO_0
+        srpm = "/tmp/abc-0.0.1-1.src.rpm"  # dummy
+        ref = [
+            "scp -p /var/lib/mock/fedora-19-x86_64/result/*.src.rpm " +
+            "jdoe@yumrepos-1.local:~jdoe/public_html/yum/fedora/19/sources",
+            "scp -p /var/lib/mock/fedora-19-x86_64/result/*.noarch.rpm " +
+            "jdoe@yumrepos-1.local:~jdoe/public_html/yum/fedora/19/x86_64" +
+            " && ssh -o ConnectTimeout=10 jdoe@yumrepos-1.local " +
+            "'cd ~jdoe/public_html/yum/fedora/19 && " +
+            "for arch in i386; do (cd $arch/ && "
+            "(for f in ../x86_64/*.noarch.rpm; do ln -sf $f ./; done)); " +
+            "done'"
+        ]
+        cs = TT.mk_cmds_to_deploy_rpms(repo, srpm, True)
+
+        self.assertTrue(cs)
+        self.assertTrue(len(cs) == 2)
+        self.assertEquals(cs[0], ref[0])
+        self.assertEquals(cs[1], ref[1])
+
+    def test_22_mk_cmds_to_deploy_rpms__arch(self):
+        repo = _REMOTE_REPO_0
+        srpm = "/tmp/abc-0.0.1-1.src.rpm"  # dummy
+        ref = [
+            "scp -p /var/lib/mock/fedora-19-x86_64/result/*.src.rpm " +
+            "jdoe@yumrepos-1.local:~jdoe/public_html/yum/fedora/19/sources",
+            "scp -p /var/lib/mock/fedora-19-x86_64/result/*.x86_64.rpm " +
+            "jdoe@yumrepos-1.local:~jdoe/public_html/yum/fedora/19/x86_64",
+            "scp -p /var/lib/mock/fedora-19-i386/result/*.i386.rpm " +
+            "jdoe@yumrepos-1.local:~jdoe/public_html/yum/fedora/19/i386"
+        ]
+        cs = TT.mk_cmds_to_deploy_rpms(repo, srpm, False)
+
+        self.assertTrue(cs)
+        self.assertTrue(len(cs) == 3)
+        self.assertEquals(cs[0], ref[0])
+        self.assertEquals(cs[1], ref[1])
+        self.assertEquals(cs[2], ref[2])
+
+    def test_24_mk_cmds_to_deploy_rpms__noarch_bdist(self):
+        repo = _REMOTE_REPO_0
+        srpm = "/tmp/abc-0.0.1-1.src.rpm"  # dummy
+        bdist = "fecora-yumrepos-19"
+        ref = [
+            "scp -p /var/lib/mock/%s-x86_64/result/*.src.rpm " % bdist +
+            "jdoe@yumrepos-1.local:~jdoe/public_html/yum/fedora/19/sources",
+            "scp -p /var/lib/mock/%s-x86_64/result/*.noarch.rpm " % bdist +
+            "jdoe@yumrepos-1.local:~jdoe/public_html/yum/fedora/19/x86_64" +
+            " && ssh -o ConnectTimeout=10 jdoe@yumrepos-1.local " +
+            "'cd ~jdoe/public_html/yum/fedora/19 && " +
+            "for arch in i386; do (cd $arch/ && "
+            "(for f in ../x86_64/*.noarch.rpm; do ln -sf $f ./; done)); " +
+            "done'"
+        ]
+        cs = TT.mk_cmds_to_deploy_rpms(repo, srpm, True, bdist)
+
+        self.assertTrue(cs)
+        self.assertTrue(len(cs) == 2)
+        self.assertEquals(cs[0], ref[0])
+        self.assertEquals(cs[1], ref[1])
+
+    def test_26_mk_cmds_to_deploy_rpms__arch_bdist(self):
+        repo = _REMOTE_REPO_0
+        srpm = "/tmp/abc-0.0.1-1.src.rpm"  # dummy
+        bdist = "fecora-yumrepos-19"
+        ref = [
+            "scp -p /var/lib/mock/%s-x86_64/result/*.src.rpm " % bdist +
+            "jdoe@yumrepos-1.local:~jdoe/public_html/yum/fedora/19/sources",
+            "scp -p /var/lib/mock/%s-x86_64/result/*.x86_64.rpm " % bdist +
+            "jdoe@yumrepos-1.local:~jdoe/public_html/yum/fedora/19/x86_64",
+            "scp -p /var/lib/mock/%s-i386/result/*.i386.rpm " % bdist +
+            "jdoe@yumrepos-1.local:~jdoe/public_html/yum/fedora/19/i386"
+        ]
+        cs = TT.mk_cmds_to_deploy_rpms(repo, srpm, False, bdist)
+
+        self.assertTrue(cs)
+        self.assertTrue(len(cs) == 3)
+        self.assertEquals(cs[0], ref[0])
+        self.assertEquals(cs[1], ref[1])
+        self.assertEquals(cs[2], ref[2])
+
+    def test_30_mk_cmds_to_update(self):
+        repo = _REMOTE_REPO_0
+        ref = [
+            "ssh -o ConnectTimeout=10 jdoe@yumrepos-1.local " +
+            "'cd ~jdoe/public_html/yum/fedora/19/x86_64 && " +
+            TT._UPDATE_REPO_METADATA + "'",
+            "ssh -o ConnectTimeout=10 jdoe@yumrepos-1.local " +
+            "'cd ~jdoe/public_html/yum/fedora/19/i386 && " +
+            TT._UPDATE_REPO_METADATA + "'"
+        ]
+        cs = TT.mk_cmds_to_update(repo)
+
+        self.assertTrue(cs)
+        self.assertTrue(len(cs) == 2)
+        self.assertEquals(cs[0], ref[0])
+        self.assertEquals(cs[1], ref[1])
+
+    def test_40_mk_cmds_to_update(self):
+        repo = _REMOTE_REPO_0
+        ref = [
+            "ssh -o ConnectTimeout=10 jdoe@yumrepos-1.local " +
+            "'mkdir -p ~jdoe/public_html/yum/fedora/19/{x86_64,i386,sources}'"
+        ]
+        cs = TT.mk_cmds_to_init__no_genconf(repo)
+
+        self.assertTrue(cs)
+        self.assertTrue(len(cs) == 1)
+        self.assertEquals(cs[0], ref[0])
+
 
 # vim:sw=4:ts=4:et:
