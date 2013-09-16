01af6962954970bf0115d72ecd019b19a8075465
Satoru SATOH <satoru.satoh@gmail.com>
2013-09-16 15:59:51 +0900

    add

---
+++ myrepo/actions/init.py
@@ -0,0 +1,76 @@
+#
+# Copyright (C) 2011 - 2013 Red Hat, Inc.
+# Red Hat Author(s): Satoru SATOH <ssato@redhat.com>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program. If not, see <http://www.gnu.org/licenses/>.
+#
+import myrepo.actions.utils as MAU
+
+import myrepo.shell as MS
+import myrepo.utils as MU
+
+import os.path
+
+
+_CMD_TEMPLATE_0 = "test -d %s || mkdir -p %s"
+
+
+def _join_dirs(prefix, dirs):
+    """
+    >>> _join_dirs("/tmp", ["abc", "def"])
+    '/tmp/{abc,def}'
+    """
+    return os.path.join(prefix, "{%s}" % ','.join(dirs))
+
+
+def prepare_0(repo):
+    """
+    Make up list of command strings to initialize repo.
+
+    :param repo: myrepo.repo.Repo instance
+
+    :return: List of command strings to deploy built RPMs.
+    """
+    MAU.assert_repo(repo)
+
+    dirs_s = _join_dirs(repo.destdir, repo.archs + ["sources"])
+    return [repo.mk_cmd("mkdir -p " + dirs_s)[0]]
+
+
+def prepare(repos):
+    """
+    Make up list of command strings to update metadata of given repos.
+    It's similar to above ``prepare_0`` but applicable to multiple repos.
+
+    :param repos: List of Repo instances
+    :param srpm: myrepo.srpm.Srpm instance
+
+    :return: List of command strings to deploy built RPMs.
+    """
+    return MU.concat(prepare_0(repo) for repo in repos)
+
+
+def run(ctx):
+    """
+    :param repos: List of Repo instances
+
+    :return: True if commands run successfully else False
+    """
+    assert "repos" in ctx, "No repos defined in given ctx!"
+
+    ps = [MS.run_async(c, logfile=False) for c in prepare(ctx["repos"])]
+    return all(MS.stop_async_run(p) for p in ps)
+
+
+# vim:sw=4:ts=4:et:
---
+++ myrepo/actions/tests/init.py
@@ -0,0 +1,84 @@
+#
+# Copyright (C) 2013 Red Hat, Inc.
+# Red Hat Author(s): Satoru SATOH <ssato at redhat.com>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+import myrepo.actions.init as TT
+import myrepo.repo as MR
+import myrepo.utils as MU
+import myrepo.tests.common as C
+
+import itertools
+import os.path
+import unittest
+
+
+class Test_00_pure_functions(unittest.TestCase):
+
+    def test_00_prepare_0__localhost(self):
+        server = MR.Server("localhost", topdir="/tmp/yum")
+        repo = MR.Repo("fedora", 19, ["x86_64", "i386"], server)
+
+        cs_expected = ["mkdir -p %s" %
+                       TT._join_dirs(repo.destdir, repo.archs + ["sources"])]
+        cs = TT.prepare_0(repo)
+
+        for c, exp in itertools.izip(cs, cs_expected):
+            self.assertEquals(c, exp)
+
+    def test_10_prepare__localhost(self):
+        server = MR.Server("localhost", topdir="/tmp/yum")
+        repos = [MR.Repo("fedora", 18, ["x86_64", "i386"], server),
+                 MR.Repo("fedora", 19, ["x86_64", "i386"], server),
+                 MR.Repo("rhel", 6, ["x86_64", ], server)]
+
+        def cs_expected_gen(repo):
+            return ["mkdir -p %s" %
+                    TT._join_dirs(repo.destdir, repo.archs + ["sources"])]
+
+        cs_expected = MU.concat(cs_expected_gen(repo) for repo in repos)
+        cs = TT.prepare(repos)
+
+        for c, exp in itertools.izip(cs, cs_expected):
+            self.assertEquals(c, exp)
+
+
+class Test_10_effecful_functions(unittest.TestCase):
+
+    def setUp(self):
+        self.workdir = C.setup_workdir()
+
+    def tearDown(self):
+        C.cleanup_workdir(self.workdir)
+
+    def test_20_run__localhost(self):
+        topdir = os.path.join(self.workdir, "yum")
+        server = MR.Server("localhost", topdir=topdir)
+        repos = [MR.Repo("fedora", 18, ["x86_64", "i386"], server),
+                 MR.Repo("fedora", 19, ["x86_64", "i386"], server),
+                 MR.Repo("rhel", 6, ["x86_64", ], server)]
+        ctx = dict(repos=repos)
+
+        def expected_dirs(repo):
+            return [os.path.join(repo.destdir, a) for a
+                    in repo.archs + ["sources"]]
+
+        self.assertTrue(TT.run(ctx))
+
+        for repo in repos:
+            for d in expected_dirs(repo):
+                self.assertTrue(os.path.exists(d), "Failed to create " + d)
+
+# vim:sw=4:ts=4:et:
