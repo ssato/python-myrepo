a1a9178f73e9923f5c28db2ba164ca42da0e4b04
Satoru SATOH <satoru.satoh@gmail.com>
2013-09-25 01:03:41 +0900

    sort out and organize test cases and complement missing test cases for myrepo.commands.build.run

--- myrepo/commands/tests/build.py
+++ myrepo/commands/tests/build.py
@@ -21,80 +21,99 @@ import myrepo.srpm as MS
 import myrepo.utils as MU
 import myrepo.tests.common as C
 
+import glob
 import itertools
 import logging
 import os.path
+import subprocess
 import unittest
+
+
+CURDIR = os.path.dirname(__file__)
+
+
+def mk_local_repos(**kwargs):
+    server = MR.Server("localhost")
+    return [MR.Repo("fedora", 18, ["x86_64", "i386"], server, **kwargs),
+            MR.Repo("fedora", 19, ["x86_64", "i386"], server, **kwargs),
+            MR.Repo("rhel", 6, ["x86_64", ], server, **kwargs)]
+
+
+def build_sample_rpm_if_not_exist():
+    rpms = glob.glob(os.path.join(CURDIR, "rpm-sample-*.src.rpm"))
+
+    if rpms:
+        return rpms[0]
+
+    subprocess.check_call(os.path.join(CURDIR, "rpm-sample-build.sh"),
+                          shell=True)
+    return glob.glob(os.path.join(CURDIR, "rpm-sample-*.src.rpm"))[0]
 
 
 class Test_00_pure_functions(unittest.TestCase):
 
+    def setUp(self):
+        self.repos = mk_local_repos()
+        self.repo = self.repos[1]  # fedora-19-*
+        self.srpm = MS.Srpm(build_sample_rpm_if_not_exist())
+        self.srpm.resolve()
+
     def test_00_prepare_0(self):
-        server = MR.Server("localhost")
-        repo = MR.Repo("fedora", 19, ["x86_64", "i386"], server)
-        srpm = MS.Srpm("/path/to/dummy-0.1.src.rpm")
-        srpm.noarch = False
+        (repo, srpm) = (self.repo, self.srpm)
+        srpm.noarch = False  # Override it.
 
         cs_expected = ["mock -r fedora-19-x86_64 %s" % srpm.path,
                        "mock -r fedora-19-i386 %s" % srpm.path]
         cs = TT.prepare_0(repo, srpm, logging.INFO)
 
-        for c, exp in itertools.izip(cs, cs_expected):
-            self.assertEquals(c, exp)
+        self.assertListEqual(cs, cs_expected)
 
     def test_02_prepare_0_noarch(self):
-        server = MR.Server("localhost")
-        repo = MR.Repo("fedora", 19, ["x86_64", "i386"], server)
-        srpm = MS.Srpm("/path/to/dummy-0.1.src.rpm")
-        srpm.noarch = True
+        (repo, srpm) = (self.repo, self.srpm)
 
         cs_expected = ["mock -r fedora-19-x86_64 %s" % srpm.path]
         cs = TT.prepare_0(repo, srpm, logging.INFO)
 
-        for c, exp in itertools.izip(cs, cs_expected):
-            self.assertEquals(c, exp)
+        self.assertListEqual(cs, cs_expected)
 
     def test_04_prepare_0_noarch__reponame(self):
-        server = MR.Server("localhost")
-        repo = MR.Repo("fedora", 19, ["x86_64", "i386"], server,
-                       reponame="fedora-custom", build_for_self=True)
-        srpm = MS.Srpm("/path/to/dummy-0.1.src.rpm")
-        srpm.noarch = True
+        repo = mk_local_repos(reponame="fedora-custom", selfref=True)[1]
+        srpm = self.srpm
 
         cs_expected = ["mock -r fedora-custom-19-x86_64 %s" % srpm.path]
         cs = TT.prepare_0(repo, srpm, logging.INFO)
 
-        for c, exp in itertools.izip(cs, cs_expected):
-            self.assertEquals(c, exp)
+        self.assertListEqual(cs, cs_expected)
 
     def test_10_prepare__noarch(self):
-        server = MR.Server("localhost")
-        repos = [MR.Repo("fedora", 18, ["x86_64", "i386"], server),
-                 MR.Repo("fedora", 19, ["x86_64", "i386"], server),
-                 MR.Repo("rhel", 6, ["x86_64", ], server)]
-        srpm = MS.Srpm("/path/to/dummy-0.1.src.rpm")
-        srpm.noarch = True
+        (repos, srpm) = (self.repos, self.srpm)
 
         cs_expected = ["mock -r fedora-18-x86_64 %s" % srpm.path,
                        "mock -r fedora-19-x86_64 %s" % srpm.path,
                        "mock -r rhel-6-x86_64 %s" % srpm.path]
         cs = TT.prepare(repos, srpm, logging.INFO)
 
-        for c, exp in itertools.izip(cs, cs_expected):
-            self.assertEquals(c, exp)
+        self.assertListEqual(cs, cs_expected)
 
 
 class Test_10_effecful_functions(unittest.TestCase):
 
     def setUp(self):
+        self.repos = mk_local_repos()
+        self.repo = self.repos[1]  # fedora-19-*
+        self.srpm = MS.Srpm(build_sample_rpm_if_not_exist())
+        self.srpm.resolve()
+
         self.workdir = C.setup_workdir()
 
     def tearDown(self):
         C.cleanup_workdir(self.workdir)
 
-    def test_20_run__localhost(self):
-        """FIXME: Add test cases for myrepo.commands.build.run."""
-        return
+    def test_20_run__localhost_noarch(self):
+        (repo, srpm) = (self.repo, self.srpm)
+        ctx = dict(repos=[repo], srpm=srpm)
+
+        self.assertTrue(TT.run(ctx))
 
 
 # vim:sw=4:ts=4:et:
