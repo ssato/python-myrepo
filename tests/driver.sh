456737b9e324f3d05bdfb06328a533232de863f8
Satoru SATOH <satoru.satoh@gmail.com>
2013-09-25 20:52:27 +0900

    update application test cases to follow changes in myrepo.cli

--- tests/driver.sh
+++ tests/driver.sh
@@ -12,11 +12,14 @@ test $# -gt 0 && tests=$@ || tests=$testsdir/*.conf
 
 # default impl (do nothing):
 check_func () { true; }
+check_func_1 () { true; }
+check_func_2 () { true; }
+check_func_3 () { true; }
 
 for f in $tests; do
     source $f
     test "x$MYREPO_TEST_ARGS" = "x" || (myrepo $MYREPO_TEST_ARGS && check_func)
-    test "x$MYREPO_TEST_ARGS_1" = "x" || (myrepo $MYREPO_TEST_ARGS_1 && check_func)
-    test "x$MYREPO_TEST_ARGS_2" = "x" || (myrepo $MYREPO_TEST_ARGS_2 && check_func)
-    test "x$MYREPO_TEST_ARGS_3" = "x" || (myrepo $MYREPO_TEST_ARGS_3 && check_func)
+    test "x$MYREPO_TEST_ARGS_1" = "x" || (myrepo $MYREPO_TEST_ARGS_1 && check_func_1)
+    test "x$MYREPO_TEST_ARGS_2" = "x" || (myrepo $MYREPO_TEST_ARGS_2 && check_func_2)
+    test "x$MYREPO_TEST_ARGS_3" = "x" || (myrepo $MYREPO_TEST_ARGS_3 && check_func_3)
 done
