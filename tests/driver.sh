9b09c1b98ba9002a67118c992dae249bccf408d7
Satoru SATOH <satoru.satoh@gmail.com>
2013-09-25 19:47:52 +0900

    update application test cases to follow changes in myrepo.{cli,config,...}

--- tests/driver.sh
+++ tests/driver.sh
@@ -8,12 +8,15 @@ myrepo () {
     PYTHONPATH=. python tools/myrepo $@
 }
 
-tests=$testsdir/*.conf
+test $# -gt 0 && tests=$@ || tests=$testsdir/*.conf
+
+# default impl (do nothing):
+check_func () { true; }
 
 for f in $tests; do
     source $f
-    test "x$MYREPO_TEST_ARGS" = "x" || myrepo $MYREPO_TEST_ARGS
-    test "x$MYREPO_TEST_ARGS_1" = "x" || myrepo $MYREPO_TEST_ARGS_1
-    test "x$MYREPO_TEST_ARGS_2" = "x" || myrepo $MYREPO_TEST_ARGS_2
-    test "x$MYREPO_TEST_ARGS_3" = "x" || myrepo $MYREPO_TEST_ARGS_3
+    test "x$MYREPO_TEST_ARGS" = "x" || (myrepo $MYREPO_TEST_ARGS && check_func)
+    test "x$MYREPO_TEST_ARGS_1" = "x" || (myrepo $MYREPO_TEST_ARGS_1 && check_func)
+    test "x$MYREPO_TEST_ARGS_2" = "x" || (myrepo $MYREPO_TEST_ARGS_2 && check_func)
+    test "x$MYREPO_TEST_ARGS_3" = "x" || (myrepo $MYREPO_TEST_ARGS_3 && check_func)
 done
