ded9fa532bfcaf633de20358b643b551172666f5
Satoru SATOH <satoru.satoh@gmail.com>
2013-09-30 20:57:31 +0900

    bump up the version

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ curdir = os.getcwd()
 sys.path.append(curdir)
 
 PACKAGE = "myrepo"
-VERSION = "0.2.99.1"
+VERSION = "0.2.99.2"
 
 # For daily snapshot versioning mode:
 if os.environ.get("_SNAPSHOT_BUILD", None) is not None:
